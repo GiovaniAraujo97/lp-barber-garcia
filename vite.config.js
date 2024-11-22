a533eb1c5c85a54503a9b672f6a8d099fe112b11
GiovaniAraujo97 <giovanijesus1997@gmail.com>
2024-11-21 21:18:00 -0300

    implementing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/lp-barber-garcia/', // Caminho base do repositório
 })
